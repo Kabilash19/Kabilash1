74604c5bc6b8994f526201e5630b1c11f8b961c9
Kabilash19 <144447776+Kabilash19@users.noreply.github.com>
2023-09-11 11:06:35 +0530

    Add files via upload

---
+++ Challenges 1/main.py
@@ -0,0 +1,9 @@
+def checkleap(year):
+  if((year % 400 == 0) or
+     (year % 100 !=0) and
+     (year % 4==0)):
+    print("leap year")
+  else:
+    print("not leap")
+year=int(input("enter the number"))
+checkleap(year)
